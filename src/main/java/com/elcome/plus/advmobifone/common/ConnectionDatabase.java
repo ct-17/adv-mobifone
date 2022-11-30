e16a4444b88f7afe2bee55574eb79e799acbc7bb
ct17 <manhcuong88c@gmail.com>
2022-11-30 22:20:51 +0700

    connect DB using jbdc template

--- src/main/java/com/elcome/plus/advmobifone/common/ConnectionDatabase.java
+++ src/main/java/com/elcome/plus/advmobifone/common/ConnectionDatabase.java
@@ -7,45 +7,45 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionDatabase {
-    private static final ConnectionDatabase instance = new ConnectionDatabase();
-
-    @Value("${db.url}")
-    private String url_db;
-
-    @Value("${db.username}")
-    private String username;
-
-    @Value("${db.password}")
-    private String password;
-
-
-    public Connection getConnection() {
-        try {
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-            Connection con = DriverManager.getConnection(url_db, decrypt(username), decrypt(password));
-            if (con != null && !con.isClosed()) {
-                con.setAutoCommit(false);
-            }
-            return con;
-        } catch (Exception e) {
-            System.out.println("Connect data base error...");
-        }
-        return null;
-    }
-
-    public String decrypt(String value) {
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword("elcomviettelvrbt");
-        config.setAlgorithm("PBEWithMD5AndDES");
-        config.setKeyObtentionIterations("1000");
-        config.setPoolSize("1");
-        config.setProviderName("SunJCE");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setStringOutputType("base64");
-        encryptor.setConfig(config);
-
-        String decrypt = encryptor.decrypt(value);
-        return decrypt;
-    }
+//    private static final ConnectionDatabase instance = new ConnectionDatabase();
+//
+//    @Value("${db.url}")
+//    private String url_db;
+//
+//    @Value("${db.username}")
+//    private String username;
+//
+//    @Value("${db.password}")
+//    private String password;
+//
+//
+//    public Connection getConnection() {
+//        try {
+//            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+//            Connection con = DriverManager.getConnection(url_db, decrypt(username), decrypt(password));
+//            if (con != null && !con.isClosed()) {
+//                con.setAutoCommit(false);
+//            }
+//            return con;
+//        } catch (Exception e) {
+//            System.out.println("Connect data base error...");
+//        }
+//        return null;
+//    }
+//
+//    public String decrypt(String value) {
+//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+//        config.setPassword("elcomviettelvrbt");
+//        config.setAlgorithm("PBEWithMD5AndDES");
+//        config.setKeyObtentionIterations("1000");
+//        config.setPoolSize("1");
+//        config.setProviderName("SunJCE");
+//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+//        config.setStringOutputType("base64");
+//        encryptor.setConfig(config);
+//
+//        String decrypt = encryptor.decrypt(value);
+//        return decrypt;
+//    }
 }
--- src/main/java/com/elcome/plus/advmobifone/dao/impl/UserDaoImpl.java
+++ src/main/java/com/elcome/plus/advmobifone/dao/impl/UserDaoImpl.java
@@ -1,22 +1,32 @@
 package com.elcome.plus.advmobifone.dao.impl;
 
-import com.elcome.plus.advmobifone.common.ConnectionDatabase;
+//import com.elcome.plus.advmobifone.common.ConnectionDatabase;
 import com.elcome.plus.advmobifone.controller.UserController;
 import com.elcome.plus.advmobifone.dao.UserDao;
 import com.elcome.plus.advmobifone.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.Types;
 
 @Repository
 public class UserDaoImpl extends AbstractDao implements UserDao {
 
     private static final Logger logger = LogManager.getLogger(UserController.class);
-    private final ConnectionDatabase connectionManager = new ConnectionDatabase();
+
+    private SimpleJdbcCall simpleJdbcCall;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public User findById(Long id) {
@@ -24,20 +34,18 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
         CallableStatement cs = null;
         ResultSet rs = null;
         try {
-            logger.info("get-user ===> id: {} ========== Start query user info ==========", id);
-            con = connectionManager.getConnection();
-            cs = con.prepareCall("{CALL WAP_AUTH.GET_USER_BY_ID(?)}");
-            cs.setInt(1, Math.toIntExact(id));
-            rs = cs.executeQuery();
-            while (rs.next()) {
-                User data = new User();
-                data.setId(rs.getLong("id"));
-                data.setUsername(rs.getString("username") != null ? rs.getString("username") : "");
-                data.setPassword(rs.getString("password") != null ? rs.getString("password") : "");
-                data.setPhone(rs.getString("phone") != null ? rs.getString("phone") : "");
-                logger.info("get-user ===> id: {} ========== output data ==========: {}", id, data);
-                return data;
-            }
+            String sqlSelect = "select * from users limit 10";
+//            logger.info("get-user ===> id: {} ========== Start query user info ==========", id);
+//            jdbcTemplate.query(sqlSelect, (Object[]) null, rs1 -> {});
+            logger.info("nmcuong da toi day");
+            simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
+                .withProcedureName("GET_USER_BY_ID")
+                .declareParameters(
+                new SqlParameter("id", Types.INTEGER));
+            logger.info("get-user ===> output data ==========: nmcuong da toi day...");
+            logger.info("get-user ===> output data ==========: {}", simpleJdbcCall.execute(new MapSqlParameterSource("id", id)));
+            User user = new User();
+            return user;
         } catch (Exception e) {
             logger.error("get-user ===> id: {} - Error get info user: {}", id, e);
             e.printStackTrace();
